016aea6b08330b4675c158d50dc78e69a925ec57
tomas-ryan-lee <contact@creative-eye.fr>
2025-01-29 17:43:10 +0100

    Bat Config

--- init-project.bat
+++ init-project.bat
@@ -1,9 +1,20 @@
-# Initialisation du Front
-git clone https://github.com/creativeeyestudio/dream-site-v3-front-web.git front
-
-# Lancement du Docker Compose
+@echo off
+echo Lancement du Docker Compose
 docker compose up -d --build
+echo.
 
-# Installation des dépendances Front
-cd ./front
+echo Installation du front
+if not exist front (
+    git clone https://github.com/creativeeyestudio/dream-site-v3-front-web.git front
+) else (
+    echo Le dossier 'front' existe deja, saut du clonage.
+)
+echo.
+
+echo Installation des dependances...
+cd front
 npm i
+echo.
+
+echo Installation terminee !
+pause
