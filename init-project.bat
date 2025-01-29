399e86908b580a6371655860dd8fd92fdfd0abb6
tomas-ryan-lee <contact@creative-eye.fr>
2025-01-29 13:27:02 +0100

    Corrections

--- init-project.bat
+++ init-project.bat
@@ -1,5 +1,9 @@
-# Initualisation du Front
+# Initialisation du Front
 git clone https://github.com/creativeeyestudio/dream-site-v3-front-web.git front
 
 # Lancement du Docker Compose
 docker compose up -d --build
+
+# Installation des dépendances Front
+cd ./front
+npm i
