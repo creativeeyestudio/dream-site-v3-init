29c3d6d1f4e2ad1748fd36a2c72e85ddae3ee2ff
tomas-ryan-lee <contact@creative-eye.fr>
2025-02-09 11:24:52 +0100

    Refonte de l'installer

--- init-project.bat
+++ init-project.bat
@@ -1,22 +1,3 @@
 @echo off
 echo Lancement du Docker Compose
 docker compose up -d --build
-echo.
-
-echo Installation du front
-if not exist front (
-    git clone https://github.com/creativeeyestudio/dream-site-v3-front-web.git front
-) else (
-    echo Le dossier 'front' existe deja, saut du clonage.
-)
-echo.
-
-echo Installation des dependances...
-cd front
-npm i
-echo.
-
-echo Installation terminee !
-pause
-
-del /f /q "%~f0"
