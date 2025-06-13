b7cf2b55a1c592aef6ceff140528476f32f1f48d
creativeeyestudio <contact@creative-eye.fr>
2025-06-13 13:05:28 +0200

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,8 +2,27 @@
 
 set -e
 
-echo "📥 Pull des images..."
-docker compose -f docker-compose.yml -f docker-compose.prod.yml pull
+echo "🔄 Mise à jour des dossiers Front & Back..."
 
-echo "♻️ Redémarrage des services..."
-docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+# FRONT update
+if [ -d "./front" ]; then
+  echo "📥 Pull du FRONT depuis le repo source..."
+  git subtree pull --prefix=front https://github.com/creativeeyestudio/dream-site-v3-front-web.git main --squash
+else
+  echo "❌ Le dossier front n'existe pas. Lance 'install.sh' d'abord."
+  exit 1
+fi
+
+# BACK update
+if [ -d "./back" ]; then
+  echo "📥 Pull du BACK depuis le repo source..."
+  git subtree pull --prefix=back https://github.com/creativeeyestudio/dream-site-v3-cms.git main --squash
+else
+  echo "❌ Le dossier back n'existe pas. Lance 'install.sh' d'abord."
+  exit 1
+fi
+
+echo "♻️ Reconstruction des conteneurs de dev..."
+docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
+
+echo "✅ Mise à jour terminée."
