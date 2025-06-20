523c780a3a0f72de3f9111b2015926f8dffa7f02
creativeeyestudio <contact@creative-eye.fr>
2025-06-20 15:37:53 +0000

    Playwright

--- back/playwright.config.ts
+++ back/playwright.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-    testDir: './tests',
+    testDir: '../tests/back/cms',
     timeout: 30_000,
     retries: 1,
     expect: { timeout: 5_000 },
     use: {
-        baseURL: 'http://localhost:3000',      // Next.js
+        baseURL: 'http://localhost:3001',      // Next.js
         headless: true,
         viewport: { width: 1280, height: 720 },
         ignoreHTTPSErrors: true,
@@ -17,12 +17,6 @@ export default defineConfig({
         {
             command: 'pnpm --filter back dev',   // Payload
             port: 3001,
-            reuseExistingServer: !process.env.CI,
-            timeout: 20_000,
-        },
-        {
-            command: 'pnpm --filter front dev',  // Next.js
-            port: 3000,
             reuseExistingServer: !process.env.CI,
             timeout: 20_000,
         },
